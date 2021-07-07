2af3287f0ccc2b791a8f8a96593563712d4103b4
MargoR96 <86987540+MargoR96@users.noreply.github.com>
2021-07-07 06:02:37 +0600

    Добавлено несколько функций

--- sanbox/src/main/java/ru/stqa/pft/sandbox/HelloWorld.java
+++ sanbox/src/main/java/ru/stqa/pft/sandbox/HelloWorld.java
@@ -2,8 +2,21 @@ package ru.stqa.pft.sandbox;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        
-        System.out.println("Hello, World");
+        hello("world");
+        hello("user");
+        System.out.println("Площадь квадрата = " + area(5)) ;
+        System.out.println("Площадь прямоугольника = " + area(5,6));
+    }
 
+    public static void hello(String somebody) {
+        System.out.println("Hello," + somebody);
+    }
+
+    public static double area (double l) {
+        double s = l*l;
+        return s;
+    }
+    public static double area (double a, double b) {
+        return a*b;
     }
 }
