6d46e471252ef2915b1014e6a288229f9a2aae8b
MargoR96 <86987540+MargoR96@users.noreply.github.com>
2021-07-07 07:30:09 +0600

    Задание 2 пункт 4

--- sanbox/src/main/java/ru/stqa/pft/sandbox/FirstProgram.java
+++ sanbox/src/main/java/ru/stqa/pft/sandbox/FirstProgram.java
@@ -4,15 +4,13 @@ import java.awt.*;
 
 public class FirstProgram {
     public static void main(String[] args) {
-        Point p1 = new Point(1,2);
-        Point p2 = new Point (5,9);
-        System.out.println("Расстояние между точками =" + distance(p1,p2));
-    }
-    public static double distance (Point p1,Point p2){
-        return Math.sqrt(((p2.x - p1.x)*(p2.x - p1.x))+((p2.y-p1.y)*(p2.y-p1.y)));
+        Point p1 = new Point(1, 2);
+        Point p2 = new Point(5, 9);
+        System.out.println("Расстояние между точками =" + p2.distance(p1));
     }
 
-
-
-
+    public static double distance(Point p1, Point p2) {
+        return Math.sqrt(((p2.x - p1.x) * (p2.x - p1.x)) + ((p2.y - p1.y) * (p2.y - p1.y)));
+    }
 }
+
